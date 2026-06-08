8c75f5c5c75ff23c08229f66d805a4a4c5a83b27
kashif5500 <mdkashifnisar5500@gmail.com>
2026-06-08 21:30:30 +0530

    MyProfile.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const[token,settoken]=useState(true)
                 <img className='w-2.5' src={assets_frontend.dropdown_icon} alt="" />
                 <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                   <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'> 
-                    <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My profile</p> 
-                    <p  onClick={()=>navigate('my-appointment')}className='hover:text-black cursor-pointer'>My Appointment</p>
+                    <p onClick={()=>navigate('myprofile')} className='hover:text-black cursor-pointer'>My profile</p> 
+                    <p  onClick={()=>navigate('myappointment')}className='hover:text-black cursor-pointer'>My Appointment</p>
                     <p onClick={()=>{settoken(false)}} className='hover:text-black cursor-pointer'>Logout</p>
                   </div>
                 </div>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,10 +17,14 @@ function Login() {
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border border-zinc-300 rounded-xl text-zinc-600 text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up'?"Create Account":"Login"}</p>
         <p>Please {state === 'Sign Up'?"Sign Up":"Log in"} to book appointment</p>
-        <div className='w-full'>
+        {
+          state === "Sign Up" &&
+          <div className='w-full'>
           <p>Full Name</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e)=>{setName(e.target.name)}} value={name} required/>
         </div>
+        }
+        
          <div className='w-full'>
           <p>Email</p>
           <input  className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(e)=>{setEmail(e.target.name)}} value={email} required/>
