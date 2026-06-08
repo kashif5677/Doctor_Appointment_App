c5f37665177520b0f06f48a9159f9f2b53db49a9
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-09 03:03:22 +0530

    top Doctor details

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,9 +8,7 @@ function Navbar() {
 
   const navigate=useNavigate()
 const [showmenu,setshowmenu]=useState(false)
-const[token,settoken]=useState(true)
-
-
+const[token,settoken]=useState(true) 
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
@@ -34,7 +32,6 @@ const[token,settoken]=useState(true)
         </NavLink>
       </ul>
       <div className='flex items-center gap-4 '> 
-
         {
           token 
           ?
@@ -45,13 +42,14 @@ const[token,settoken]=useState(true)
                 <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                   <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'> 
                     <p onClick={()=>navigate('myprofile')} className='hover:text-black cursor-pointer'>My profile</p> 
-                    <p  onClick={()=>navigate('myappointment')}className='hover:text-black cursor-pointer'>My Appointment</p>
+                    <p onClick={()=>navigate('myappointment')}className='hover:text-black cursor-pointer'>My Appointment</p>
                     <p onClick={()=>{settoken(false)}} className='hover:text-black cursor-pointer'>Logout</p>
                   </div>
                 </div>
-                </div>)
+                </div>)  
          :( <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer'>create account</button>)
         }
+        <img src={assets} alt="" />
       </div>
     </div>
   )
