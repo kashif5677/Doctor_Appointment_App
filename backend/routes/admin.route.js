0dc54804890b8a7433d637dc93cbfe9fd106b9b5
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-11 01:20:23 +0530

    admin.routes

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -3,3 +3,7 @@ import { addDoctor } from '../controllers/admin.controller'
 import upload from '../middlewares/multer'
 
 const adminRouter = express.Router()
+
+adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
+
+export default adminRouter  
