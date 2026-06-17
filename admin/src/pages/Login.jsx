5b09728f1b35d2c64e759d7ee23a064c9918a0d2
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-18 01:25:42 +0530

    login.jsx

---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import {assets} from '../../../admin/src/assets/assets.js'
+
+function Login() {
+
+    const [state,setState]=useState('Admin')
+
+
+  return (
+    <form className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-2xl border-zinc-300 rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p><span>{state}</span>Login</p>
+        <div>
+            <p>Email</p>
+            <input type="email" required />
+        </div>
+        <div>
+            <p>Password</p>
+            <input type="password" required />
+        </div>
+        <button>Login</button>
+      </div>
+    </form>
+  )
+}
+
+export default Login
