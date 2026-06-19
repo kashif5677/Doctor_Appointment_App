43aa21ee0bf4348026871dee4cef9b99f80fd092
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-19 22:14:33 +0530

    adding primary color

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   return (
     <form className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-2xl border-zinc-300 rounded-xl text-zinc-600 text-sm shadow-lg'>
-        <p><span>{state}</span>Login</p>
+        <p className='text-2xl font-semibold m-auto'>< span className='text-primary'>{state}</span>Login</p>
         <div>
             <p>Email</p>
             <input type="email" required />
