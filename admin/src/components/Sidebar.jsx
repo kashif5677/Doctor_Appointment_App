d41791cb098797439aa47782be2e640148949d2c
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-21 02:27:34 +0530

    sidebar.jsx

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import {useContext} from 'react'
+import {AdminContext} from '../context/AdminContext'
+import {NavLink} from 'react-router-dom'
+import {assets} from '../assets/assets.js'
 
 function Sidebar() {
+
+    const {aToken}=useContext(AdminContext)
+
   return (
     <div>
-      sidebar
-      
+      {
+        aToken && <ul>
+            <NavLink to={'/admin-dashboard'}>
+                <img src={assets.home_icon} alt="" />
+                <p>Dashboard</p>
+            </NavLink>
+            <NavLink to={'/all-appointments'}>
+                <img src={assets.appointment_icon} alt="" />
+                <p>Appointments</p>
+            </NavLink>
+            <NavLink to={'/add-doctor'}>
+                <img src={assets.add_icon} alt="" />
+                <p>Add Doctors</p>
+            </NavLink>
+            <NavLink to={'/doctor-list'}>
+                <img src={assets.people_icon} alt="" />
+                <p>Doctors</p>
+            </NavLink>
+        </ul>
+      }
+
     </div>
   )
 }
