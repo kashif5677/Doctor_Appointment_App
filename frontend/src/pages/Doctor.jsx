ba58c742bd75625befd44bed11becc68ac121747
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-22 02:11:00 +0530

    adding file in addDoctor

--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -28,7 +28,7 @@ function Doctor() {
     <p>Browse through our extension list of trusted doctors</p>
       <div className='flex flex-col sm:flex-row items-start gap-2 mt-5'>
        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filters</button>
-        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden'} `}>
+        <div className={` flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden'} sm:flex `}>
           <p onClick={()=>speciality === 'General Physician' ? navigate('/doctors'):navigate('/doctors/General Physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General Physician" ?'bg-indigo-100 text-black':''}`}>General Physician</p>
           <p onClick={()=>speciality === 'Gynecologist' ? navigate('/doctors'):navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ?'bg-indigo-100 text-black':''}`}>Gynecologist</p>
           <p onClick={()=>speciality === 'Dermatologist' ? navigate('/doctors'):navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ?'bg-indigo-100 text-black':''}`}>Dermatologist</p>
