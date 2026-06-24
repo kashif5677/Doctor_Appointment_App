72ff2f1b33de11c5fdce1f8a1608e36e0d99f716
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-25 02:38:45 +0530

    adding image

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -17,6 +17,9 @@ function AppContextProvider(props) {
     }
     return false
   })
+  const [userData, setUserData] = useState(false)
+
+
 
   const getDoctorsData = async () => {
     try {
@@ -32,17 +35,43 @@ function AppContextProvider(props) {
     }
   }
 
+  const loadUserProfileData = async () => {
+    try {
+
+      const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+
+      if (data.success) {
+        setUserData(data.user)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
   const value = {
     doctors,
     currencySymbol,
     token,
     setToken,
-    backendUrl
+    backendUrl,
+    userData, setUserData,
+    loadUserProfileData
   }
 
   useEffect(() => {
     getDoctorsData()
   }, [])
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData()
+    } else {
+      setUserData(false)
+    }
+  }, [token])
 
   return (
     <AppContext.Provider value={value}>
