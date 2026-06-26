d7063f5cb534aab2a42e9d48b4eb7106357ea0ab
kashif5677 <mdkashifnisar5677@gmail.com>
2026-06-26 23:55:26 +0530

    payment

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -5,7 +5,7 @@ const appointmentSchema = new mongoose.Schema({
     docId: { type: String, required: true },
     slotTime: { type: String, required: true },
     slotDate: { type: String, required: true },
-    userData: { type: Object, required: true },
+    // userData: { type: Object, required: true },
     docData: { type: Object, required: true },
     amount: { type: Number, required: true },
     date: { type: Number, required: true },
