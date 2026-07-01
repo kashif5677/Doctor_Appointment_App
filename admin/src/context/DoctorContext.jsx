1f1f1c5644e6ad83e00bd3a2d593abf626cff520
Md Kashif Nisar <mdkashifnisar5677@gmail.com>
2026-07-01 21:47:32 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = https://doctor-appointment-backend-cxi7.onrender.com
+    const backendUrl = 'https://doctor-appointment-backend-cxi7.onrender.com'
 
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : null)
     const [appointments, setAppointments] = useState([])
